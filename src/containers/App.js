756691844b22cc5d4f4b34618fe353d7f2c881bb
Woji <lubowicki.w@gmail.com>
2018-04-21 22:27:33 +0200

    task completed

---
+++ src/containers/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import uuid from 'uuid';
+import style from './App.css';
+import Title from '../components/Title'
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data: [{
+                id: 1,
+                text: 'clean room'
+                }, {
+                id: 2,
+                text: 'wash the dishes'
+                }, {
+                id: 3,
+                text: 'feed my cat'
+            }]
+        };
+    }
+    addTodo(val){
+        const todo = {
+            text: val,
+            id: uuid.v4(),
+        };
+        const data = [...this.state.data, todo];
+        this.setState({data});
+    }
+    removeTodo(id) {
+    const remainder = this.state.data.filter(todo => todo.id !== id);
+    this.setState({data: remainder});
+    }
+    render() {
+        return (
+            <div className={style.TodoApp}>
+                <Title data={this.state.data}/>
+                {this.state.data.map((todo) => {
+                    return <div key={todo.id} onClick={() => this.removeTodo(todo.id)}>{todo.text}</div>
+                })}
+            </div>
+        );
+    }
+}
+
+export default App;
